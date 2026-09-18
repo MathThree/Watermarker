17ca305b4d2c558f8589546d2142c271b28449df
Mathieu TIMMERMAN <timmer.mathieu@gmail.com>
2026-09-18 16:30:55 +0200

    First commit

---
+++ Watermarker/Watermarker/ImageFile.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp.Drawing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Processing;
+using System.Runtime.CompilerServices;
+
+namespace Watermarker
+{
+    internal class ImageFile
+    {
+        private string _filePath;
+        public string FileName { get; private set; }
+        public string FileExtension { get; private set; }
+        private Image<Rgba32> OriginalImage;
+        private Image<Rgba32> WatermarkedImage;
+        private const int MaxSideLength = 20;
+
+        public ImageFile()
+        {
+            _filePath = string.Empty;
+            FileName = string.Empty;
+            FileExtension = string.Empty;
+            OriginalImage = null;
+        }
+
+        public string FilePath
+        {
+            get => _filePath;
+            set
+            {
+                // Check is a valid file path and if the file exists
+                if (System.IO.File.Exists(value))
+                {
+                    _filePath = value;
+                    FileName = System.IO.Path.GetFileName(value);
+                    FileExtension = System.IO.Path.GetExtension(value);
+                    OriginalImage?.Dispose();
+                    OriginalImage = Image.Load<Rgba32>(_filePath);
+                }
+            }
+        }
+
+        public void SaveWatermarkedImage(string watermark)
+        {
+            // Save the watermarked image to a new file
+            string directory = System.IO.Path.GetDirectoryName(_filePath);
+            string newFileName = $"{System.IO.Path.GetFileNameWithoutExtension(_filePath)}_watermarked{FileExtension}";
+            string newFilePath = System.IO.Path.Combine(directory, newFileName);
+            WatermarkedImage.Save(newFilePath);
+            Console.WriteLine($"Watermarked image saved to: {newFilePath}");
+
+            // Open the new image file in the default image viewer
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(newFilePath) { UseShellExecute = true });
+        }
+        public Image<Rgba32> GetWatermarkedImageType1(string watermark, float alpha)
+        {
+            WatermarkedImage?.Dispose();
+            WatermarkedImage = OriginalImage.Clone();
+
+            // Add watermark to the image
+            Font baseFont = SystemFonts.CreateFont("Arial", 96);
+
+            FontRectangle textSize = TextMeasurer.MeasureAdvance(watermark, new TextOptions(baseFont));
+
+            double diagonalLength = Math.Sqrt(WatermarkedImage.Width * WatermarkedImage.Width + WatermarkedImage.Height * WatermarkedImage.Height);
+
+            Font adaptedFont = SystemFonts.CreateFont("Arial", (float)(96 * diagonalLength / textSize.Width) * .85f);
+
+            RichTextOptions textOptions = new RichTextOptions(adaptedFont)
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+
+            PointF startPoint = new PointF(WatermarkedImage.Width / 2, WatermarkedImage.Height / 2);
+            PointF endPoint = new PointF(WatermarkedImage.Width, WatermarkedImage.Height);
+            IPath diagonalPath = new SixLabors.ImageSharp.Drawing.Path(new LinearLineSegment(startPoint, endPoint));
+
+            WatermarkedImage.Mutate(ctx => ctx.Paint(CanvasAction =>
+            {
+                CanvasAction.DrawText(textOptions, watermark, diagonalPath, Brushes.Solid(Color.White.WithAlpha(alpha)), pen: null);
+            }));
+
+            return WatermarkedImage;
+        }
+
+        public Image<Rgba32> GetWatermarkedImageType2(string watermark, float alpha)
+        {
+            WatermarkedImage?.Dispose();
+            WatermarkedImage = OriginalImage.Clone();
+
+            // Add watermark to the image
+            Font baseFont = SystemFonts.CreateFont("Arial", 96);
+
+            FontRectangle textSize = TextMeasurer.MeasureAdvance(watermark, new TextOptions(baseFont));
+
+            double diagonalLength = Math.Sqrt(WatermarkedImage.Width * WatermarkedImage.Width + WatermarkedImage.Height * WatermarkedImage.Height);
+
+            Font adaptedFont = SystemFonts.CreateFont("Arial", (float)(96 * diagonalLength / textSize.Width) * .85f);
+
+            RichTextOptions textOptions = new RichTextOptions(adaptedFont)
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+
+            PointF startPoint = new PointF(WatermarkedImage.Width / 2, WatermarkedImage.Height / 2);
+            PointF endPoint = new PointF(WatermarkedImage.Width, WatermarkedImage.Height);
+            IPath diagonalPath = new SixLabors.ImageSharp.Drawing.Path(new LinearLineSegment(startPoint, endPoint));
+
+            WatermarkedImage.Mutate(ctx => ctx.Paint(CanvasAction =>
+            {
+                CanvasAction.DrawText(textOptions, watermark, diagonalPath, Brushes.Solid(Color.White.WithAlpha(alpha)), pen: null);
+            }));
+
+            return WatermarkedImage;
+        }
+
+        public void Dispose()
+        {
+            OriginalImage?.Dispose();
+            WatermarkedImage?.Dispose();
+        }
+    }
+}
---
+++ Watermarker/Watermarker/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Watermarker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ImageFile imageFile;
+        public MainWindow()
+        {
+            InitializeComponent();
+            imageFile = new ImageFile();
+        }
+
+        private void Display_Watermarked_Image(Image<Rgba32> watermarkedImage)
+        {
+            using (var memoryStream = new System.IO.MemoryStream())
+            {
+                watermarkedImage.SaveAsPng(memoryStream);
+
+                memoryStream.Seek(0, System.IO.SeekOrigin.Begin);
+                BitmapImage bitmapImage = new BitmapImage();
+
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+                WatermarkedImage.Source = bitmapImage;
+            }
+        }
+
+        private void Button_Click_Watermark1(object sender, RoutedEventArgs e)
+        {
+            if (imageFile.FilePath != null && imageFile.FilePath != string.Empty)
+            {
+                string watermarkText = WatermarkTextBox.Text;
+                using (Image<Rgba32> watermarkedImage = imageFile.GetWatermarkedImageType1(watermarkText, (float)(AlphaSlider.Value / 100)))
+                {
+                    Display_Watermarked_Image(watermarkedImage);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an image file first.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click_Watermark2(object sender, RoutedEventArgs e)
+        {
+            if (imageFile.FilePath != null && imageFile.FilePath != string.Empty)
+            {
+                string watermarkText = WatermarkTextBox.Text;
+                using (Image<Rgba32> watermarkedImage = imageFile.GetWatermarkedImageType2(watermarkText, (float)(AlphaSlider.Value / 100)))
+                {
+                    Display_Watermarked_Image(watermarkedImage);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an image file first.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click_Open_File(object sender, RoutedEventArgs e)
+        {
+            // Open a file explorer dialog to select an image file
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.png)|*.jpg;*.jpeg;*.png|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                imageFile.FilePath = filePath;
+                Image<Rgba32> image = SixLabors.ImageSharp.Image.Load<Rgba32>(filePath);
+                // Display the image in the target image control
+                TargetImage.Source = new BitmapImage(new Uri(filePath));
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AlphaSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (AlphaValueTextBlock == null) return;
+            AlphaValueTextBlock.Text = ((float)(AlphaSlider.Value / 100)).ToString("0.00");
+        }
+    }
+}
